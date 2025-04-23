5647810fa0bed6d2bbc0ee926b3ef400e2181d7c
wonderenlee <klphumisithikul@outlook.com>
2025-04-23 14:29:15 -0400

    add subclass Book under parent class Media

--- app.js
+++ app.js
@@ -2,97 +2,101 @@
 class Media {
     // create constructor for parent class
     constructor(title) {
-      this._title = title;
-      this._isCheckedOut = false;
-      this._ratings = [];
+        this._title = title;
+        this._isCheckedOut = false;
+        this._ratings = [];
     }
-    
+
     // create getters
     get title() {
-      return this._title;
+        return this._title;
     }
-  
+
     get isCheckedOut() {
-      return this._isCheckedOut;
+        return this._isCheckedOut;
     }
-  
+
     get ratings() {
-      return this._ratings;
+        return this._ratings;
     }
-  
+
     // create setter
     set isCheckedOut(value) {
-      this._isCheckedOut = value;
+        this._isCheckedOut = value;
     }
 
     // create methods
-    // create method toggleCheckOutStatus that changes the value saved to the _isCheckedOut property    
+    // create method toggleCheckOutStatus that changes the value saved to the _isCheckedOut property
     toggleCheckOutStatus() {
         if (this_.isCheckedOut === true) {
-            return this._isChecekedOut = false;
+            return (this._isChecekedOut = false);
         } else {
-            return this._isCheckedOut = true;
+            return (this._isCheckedOut = true);
         }
     }
-      
+
     getAverageRating() {
-        let ratingsSum = this.ratings.reduce((currentSum, rating) => currentSum + rating, 0);
+        let ratingsSum = this.ratings.reduce(
+            (currentSum, rating) => currentSum + rating,
+            0
+        );
         const ratingsLength = this.ratings.length;
         ratingsAvg = ratingsSum / ratingsLength;
         return ratingsAvg;
-      }
+    }
 
     addRating(value2) {
         this.addRating.push(value2);
     }
-  }
-  /*
-  // create child class Book
-  class Book extends Media {
-    constructor(author, pages) {
-      super(title, ratings, isCheckedOut);
-      this._author;
-      this._pages;
+}
+
+// create child class Book
+class Book extends Media {
+    constructor(author, title, pages) {
+        super(title);
+        this._author;
+        this._pages;
     }
-  
+
     get author() {
-      return this._author;
+        return this._author;
     }
-  
+
     get pages() {
-      return this._pages;
+        return this._pages;
     }
-  }
-  
-  // create child class Movie
-  class Movie extends Media {
+}
+
+// create child class Movie
+class Movie extends Media {
     constructor(director, runTime) {
-      super(title, ratings, isCheckedOut);
-      this._director;
-      this._runTime;
+        super(title, ratings, isCheckedOut);
+        this._director;
+        this._runTime;
     }
-  
+
     get director() {
-      return this._director;
+        return this._director;
     }
-  
+
     get runTime() {
-      return this._runTime;
+        return this._runTime;
     }
-  }
-  
-  // create child class CD
-  class CD extends Media {
+}
+
+// create child class CD
+class CD extends Media {
     constructor(artist, pages) {
-      super(title, ratings, isCheckedOut);
-      this._artist;
-      this._pages;
+        super(title, ratings, isCheckedOut);
+        this._artist;
+        this._pages;
     }
-  
+
     get artist() {
-      return this._artist;
+        return this._artist;
     }
-  
+
     get pages() {
-      return this._pages;
-    } */
+        return this._pages;
+    }
+}
